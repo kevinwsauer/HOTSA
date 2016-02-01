56a98373c15bd4c1a34bef1aefc24c8ef0f33570
Kevin Sauer <kevinwsauer@gmail.com>
2016-02-01 14:34:10 -0600

    Removes the annual meeting info from the announcement message.

--- _includes/announcements.php
+++ _includes/announcements.php
@@ -1,4 +1,4 @@
 <aside id="announcements">
 <h3>Announcements</h3>
-<p>There is a mandatory annual members meeting on Saturday, January 30<sup>th</sup> at 10:00am, at the range. We have several issues to discuss, as well as, voting for a few elect positions.</p>
+<p>There are no announcements at this time.</p>
 </aside><!-- #announcements -->
