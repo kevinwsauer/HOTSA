155b373b9fdf90a2b8d016e9b7a47ba3e37d0168
Kevin Sauer <kevinwsauer@gmail.com>
2016-02-23 16:44:44 -0600

    Adds emails for the Officials & Directors.

--- officials-directors.php
+++ officials-directors.php
@@ -28,49 +28,44 @@ $title = " - Officials &amp; Directors"; ?>
 <th colspan="2">Officials: </th>
 </tr>
 <tr>
-<td>President: </td>
-<td>Jack Garrett</td>
+<td>President:</td>
+<td>Jack Garrett<br>email: jgarrett@centurylink.net</td>
 </tr>
 <tr>
-<td>Vice President: </td>
-<td>David Barnes</td>
+<td>Vice President:</td>
+<td>David Barnes<br>email: highwall@hotmail.com</td>
 </tr>
 <tr>
-<td>Secretary: </td>
-<td>Hal Vinson</td>
+<td>Secretary:</td>
+<td>Hal Vinson<br>email: hvinson2@verizon.net</td>
 <tr>
 </table>
 <table id="directors">
 <tr>
 <th colspan="2">Discipline Directors: </th>
 </tr>
-<tr>
-<td>BPCR: </td>
-<td>Kenny Tebbe</td>
+<td>BPCR:</td>
+<td>Kenny Tebbe<br>email: ken_tebbe@baylor.edu</td>
 </tr>
 <tr>
-<td>High Power: </td>
-<td>Jack Garrett</td>
+<td>High Power:<br>Jack Garrett</td>
+<td>Jack Garrett<br>email: jgarrett@centurylink.net</td>
 </tr>
 <tr>
-<td>22 BPCR: </td>
-<td>Walt Ballew</td>
+<td>22 BPCR:</td>
+<td>Walt Ballew<br>email: </td>
 </tr>
 <tr>
-<td>Clays &amp;<br>
-VBR (Varmint Bench Rest): </td>
-<td>Denny Henderson<br>
-David Brady</td>
+<td>Clays &amp;<br>VBR (Varmint Bench Rest):</td>
+<td>Denny Henderson<br>email: drh15t@aol.com<br>David Brady<br>email: chempura@flash.net</td>
 </tr>
 <tr>
-<td>Lever Action: </td>
-<td>Curtis Smith</td>
+<td>Lever Action:</td>
+<td>Curtis Smith<br>email: curtisds11@yahoo.com</td>
 </tr>
 <tr>
-<td>Directors at large: </td>
-<td>Sean Stratton<br>
-David Brady<br>
-Humberto "Bert" Hernandez</td>
+<td>Directors at large:</td>
+<td>Sean Stratton<br>David Brady<br>Humberto "Bert" Hernandez</td>
 </tr>
 </table>
 </article><!-- #officials-directors -->
