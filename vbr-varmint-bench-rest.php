bdc0fc7afa61d23b7870bcf390494db4b1f919a1
Kevin Sauer <kevinwsauer@gmail.com>
2016-02-23 10:35:43 -0600

    Modifies the VBR page to float the silhouette details into a two column layout on desktop.

--- vbr-varmint-bench-rest.php
+++ vbr-varmint-bench-rest.php
@@ -26,12 +26,15 @@ $title = " - VBR: Varmint Bench Rest"; ?>
 <p>We are super excited to bring a new sport to our shooting range.  Kenny Tebbe was inspired after seeing a long range shooting event on the outdoor channel and a spark was ignited that is starting to burn a little bigger.  Judging by the number of inquiries and the talk at the local gun shop (of course,that's Keith's Hardware), it will be a blazing wildfire in no time.</p>
 <p>This is a sport shooting little-bitty steel targets at long range.  We have modified the targets to approximate one MOA at all distance which means scaling down each target a bit (or a lot).</p> 
 <p>So here are some target sizes to give you an idea of how it might look.</p>
-<p class="silhouettes-details">
-<img class="prairiedog" src="_images/silhouettes/prairiedog.png" alt="Prairie Dog">200 meters: Prairie dog is 2" wide x about 3-1/2" tall.<br> 
-<img class="armadillo" src="_images/silhouettes/armadillo.png" alt="Armadillo">300 meters: an armadillo about 3" long and only 2-1/2" tall.<br>
-<img class="coyote" src="_images/silhouettes/coyote.png" alt="Coyote">385 meters: Coyote, sitting.  We'll just say he's a real little triangle and might just turn out to be the most challenging target due to his really narrow nose and wide base.<br>
-<img class="boar" src="_images/silhouettes/boar.png" alt="Boar">500: The good old wild boar, but he's a tiny little fart. Only 5" wide x about 4" tall.</p><!-- .silhouettes-details -->
-<p>These are just some dimensions to give you an idea of the challenge. We have not finalized the targets yet so don't be surprised if it doesn't look just like this when you get there.  This is blast to shoot and we are trying to make it where about anyone that has a good accurate varmint rifle can come out and shoot.  Oh, you only have your big game rifle?  Well, you may have to shoot paper targets so as to preserve the targets, but I bet we can find a place for you.</p>  
+<div class="row">
+<div class="silhouette-details"><img class="prairiedog" src="_images/silhouettes/prairiedog.png" alt="Prairie Dog">200 meters: Prairie dog is 2" wide x about 3-1/2" tall.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="armadillo" src="_images/silhouettes/armadillo.png" alt="Armadillo">300 meters: an armadillo about 3" long and only 2-1/2" tall.</div><!-- .silhouette-details -->
+</div><!-- .row -->
+<div class="row">
+<div class="silhouette-details"><img class="coyote" src="_images/silhouettes/coyote.png" alt="Coyote">385 meters: Coyote, sitting.  We'll just say he's a real little triangle and might just turn out to be the most challenging target due to his really narrow nose and wide base.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="boar" src="_images/silhouettes/boar.png" alt="Boar">500: The good old wild boar, but he's a tiny little fart. Only 5" wide x about 4" tall.</div><!-- .silhouette-details -->
+</div><!-- .row -->
+<p class="clear">These are just some dimensions to give you an idea of the challenge. We have not finalized the targets yet so don't be surprised if it doesn't look just like this when you get there.  This is blast to shoot and we are trying to make it where about anyone that has a good accurate varmint rifle can come out and shoot.  Oh, you only have your big game rifle?  Well, you may have to shoot paper targets so as to preserve the targets, but I bet we can find a place for you.</p><!-- .clear -->  
 <p>I will update this section as time permits, but keep checking in to see the upcoming fun.</p>
 <p>I will have details on rules and match format as soon as possible.</p>
 </article><!-- #vbr -->
