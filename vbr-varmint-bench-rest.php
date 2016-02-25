c5636334229cea4429607c4285728c605093728c
Kevin Sauer <kevinwsauer@gmail.com>
2016-02-25 14:33:31 -0600

    Corrections to the images and related styling. Also updates some of the info for the chicken and pig silhouettes.

--- vbr-varmint-bench-rest.php
+++ vbr-varmint-bench-rest.php
@@ -25,23 +25,23 @@ $title = " - VBR: Varmint Bench Rest"; ?>
 <h2>VBR: Varmint Bench Rest</h2>
 <p>We are super excited to bring a new sport to our shooting range.  Kenny Tebbe was inspired after seeing a long range shooting event on the outdoor channel and a spark was ignited that is starting to burn a little bigger.  Judging by the number of inquiries and the talk at the local gun shop &#40;of course,that's Keith's Hardware&#41;, it will be a blazing wildfire in no time.</p>
 <p>This is a sport shooting little-bitty steel targets at long range.  We have modified the prairie dog, armadillo, coyote and wild boar targets to approximate one MOA at all distance which means scaling down each target a bit &#40;or a lot&#41;.</p> 
-<p>So here are the target sizes to give you an idea of how it might look.</p>
+<p>So here are the prairie dog, armadillo, coyote and wild boar target sizes to give you an idea of how it might look.</p>
 <div class="row">
-<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/prairiedog.png" alt="Prairie Dog">200 meters: Prairie dog is 2" wide x about 3-1/2" tall.</div><!-- .silhouette-details -->
-<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/armadillo.png" alt="Armadillo">300 meters: An armadillo about 3" long and only 2-1/2" tall.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/prairiedog.png" alt="Prairie Dog" height="112" width="167">200 meters: The prairie dog, standing is 2" wide x about 3-1/2" tall.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/armadillo.png" alt="Armadillo" height="112" width="167">300 meters: An armadillo about 3" long and only 2-1/2" tall.</div><!-- .silhouette-details -->
 </div><!-- .row -->
 <div class="row">
-<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/coyote.png" alt="Coyote">385 meters: Coyote, sitting.  We'll just say he's a real little triangle and might just turn out to be the most challenging target due to his really narrow nose and wide base.</div><!-- .silhouette-details -->
-<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/wildboar.png" alt="Boar">500: The good old wild boar, but he's a tiny little fart. Only 5" wide x about 4" tall.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/coyote.png" alt="Coyote" height="112" width="167">385 meters: The coyote, sitting. We'll just say he's a real little triangle and might just turn out to be the most challenging target due to his really narrow nose and wide base.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/wildboar.png" alt="Boar" height="112" width="167">500: The good old wild boar, but he's a tiny little fart. Only 5" wide x about 4" tall.</div><!-- .silhouette-details -->
 </div><!-- .row -->
-<p class="clear">In order to add additional skills and fun into this new discipline and utilize our new 600 and 750 yard berms, we are adding 2 additional targets that will utilize the existing prairie chicken and pig silhouette targets. The prairie chicken and pig are about 2 MOA each at their respective distances.</p><!-- .clear -->
-<p>So here are the target sizes to give you an idea of how it might look.</p>
+<p class="clear">In order to add additional skills and fun into this new discipline and utilize our new 600 yard and 750 yard berms, we are adding 2 additional targets that will utilize the existing metallic silhouette prairie chicken and pig targets. The prairie chicken and pig are about 2 MOA each at their respective distances.</p><!-- .clear -->
+<p>So here are the prairie chicken and pig target sizes to give you an idea of how it might look.</p>
 <div class="row">
-<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/prairiechicken.png" alt="Chicken">600 yards: The prairie chicken.</div><!-- .silhouette-details -->
-<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/pig.png" alt="Boar">750 yards: The pig.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/prairiechicken.png" alt="Prairie Chicken" height="112" width="167">600 yards: The prairie chicken is 13" wide x 11" tall.</div><!-- .silhouette-details -->
+<div class="silhouette-details"><img class="silhouettes" src="_images/silhouettes/pig.png" alt="Pig" height="112" width="167">750 yards: The pig is 22" wide x 14" tall.</div><!-- .silhouette-details -->
 </div><!-- .row -->
-<p class="clear">The targets will be a "hanging target" and the spotter will score "hit" or "miss". There will be 10 shots for score at each target. Therefore, the match will now be 60 rounds <span class="note">&#40;10 rounds &#64; prairie dog, armadillo, coyote, wild boar, prairie chicken and pig&#41;</span>. The entry fee will be &#36;35.00.</p>
-<p>These are just some dimensions to give you an idea of the challenge. We have not finalized the targets yet so don't be surprised if it doesn't look just like this when you get there.  This is blast to shoot and we are trying to make it where about anyone that has a good accurate varmint rifle can come out and shoot.  Oh, you only have your big game rifle?  Well, you may have to shoot paper targets so as to preserve the targets, but I bet we can find a place for you.</p><!-- .clear -->  
+<p class="clear">The prairie chicken and pig targets will be "hanging targets" and the spotter will score "hit" or "miss". There will be 10 shots for score at each target. Therefore, the match will now be 60 rounds <span class="note">&#40;10 rounds &#64; prairie dog, armadillo, coyote, wild boar, prairie chicken and pig&#41;</span>. The entry fee will be &#36;35.00.</p>
+<p>These are just some dimensions to give you an idea of the challenge. This is blast to shoot and we are trying to make it where about anyone that has a good accurate rifle can come out and shoot.</p><!-- .clear -->  
 </article><!-- #vbr -->
 </section><!-- #main -->
 <!-- End Main Section -->
