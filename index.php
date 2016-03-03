cf773b3c5108cf80d91802e57adccefb497db026
Kevin Sauer <kevinwsauer@gmail.com>
2016-03-03 16:17:51 -0600

    Adds featured section to the main page.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- Declare Description, Keywords and Title -->
 <?php 
-$description = "The Heart of Texas Silhouette Association, also known as HOTSA, is a private, members only, rifle range and gun club located just an hour drive from either the Dallas/Ft. Worth area or Waco in the heart of Central Texas.";
+$description = "The Heart of Texas Silhouette Association, also known as HOTSA, is a private, members only, rifle range and gun club located in the heart of Central Texas.";
 $keywords = "Silhouette Shooting, Shooting Range, Gun Club, Long Range Shooting, NRA Sanctioned, Black Powder, Central Texas, BPCR, High Power, 22, Varmit, Silhouette";
 $title = " - Home"; ?>
 <!-- Include Header File -->
@@ -23,13 +23,19 @@ $title = " - Home"; ?>
 <section id="main" class="cf" role="main">
 <article id="home" role="contentinfo">
 <h2>Welcome</h2>
-<p>The Heart of Texas Silhouette Association, also known as HOTSA, is a private, members only, rifle range and gun club located just an hour drive from either the Dallas/Ft. Worth area or Waco in the heart of Central Texas.</p>
+<p>The Heart of Texas Silhouette Association, also known as HOTSA, is a private, members only, rifle range and gun club located a little over an hour drive from either the Dallas/Ft. Worth area or Waco in the heart of Central Texas.</p>
 <p>HOTSA is dedicated to the sport of silhouette; BPCR, High Power, 22, and Varmint shooting sports. Our range features berms from 25 to 750 yards that accommodate pistol shooting to long range rifle shooting. Our main field boasts berms at 200, 300, 385 &amp; 500 meters for all silhouette disciplines. Our members have full access to the range year round regardless of match schedules with live fire allowed during daylight hours only; <strong>no night shooting allowed</strong>.</p>
 <p>HOTSA holds NRA sanctioned BPCR silhouette matches the first Saturday of each month, and 22 BPCRA matches the third Saturday of each month. These matches are weather permitting and are usually a 40 shot format. In the fall, registration begins about 8:00am with the match commencing at 9:00am.  In the summer, the match will begin about 8:30am. Please check the website, or contact the match director prior to the match to ensure the match will take place, as weather and other factors can occasionally result in canceling of a scheduled match. Check the <a href="match-schedule.php" title="Match Schedule">Match Schedule</a> page for additional shooting disciplines matches and directors contact info.</p>
 <p>For details on how to find our range click the <a href="directions-map.php" title="Directions & Map">Directions &amp; Map</a> link for directions.</p>
 </article><!-- #home -->
 <!-- Include Announcements File -->
 <?php include("_includes/announcement-redirect.php"); ?>
+<section class="featured background" id="featured">
+<div class="featured-container">
+<h2>Range Featured</h2>
+<p>The Heart of Texas Silhouette Association's shooting range is featured in the background of The Legendary Shiloh Sharps Rifle Company's advertisement for their black powder rifles.</p>
+</div><!-- .featured-container -->
+</section><!-- #featured .featured .background -->
 </section><!-- #main .cf -->
 <!-- End Main Section -->
 <!-- Include Footer File -->
