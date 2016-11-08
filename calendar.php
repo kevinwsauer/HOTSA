6cfb84beceb64b28e6bd0f58de2ac9e6a4af29bf
Kevin Sauer <kevinwsauer@users.noreply.github.com>
2016-11-08 10:58:57 -0600

    Changed color of calendar entries to blue

--- calendar.php
+++ calendar.php
@@ -26,7 +26,7 @@ $title = " - Calendar"; ?>
 <h2>Calendar</h2>
 <h3>HOTSA</h3>
 <div class="responsiveCal">
-<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=hotsa.org%40gmail.com&amp;color=%23711616&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=hotsa.org%40gmail.com&amp;color=%23286ea0&amp;ctz=America%2FChicago" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 </div><!-- .responsiveCal -->
 </article><!-- #calendar -->
 </section><!-- #main -->
