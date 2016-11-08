089ae3c205215b6f503087c68f20aeec94adfccd
Kevin Sauer <kevinwsauer@gmail.com>
2016-11-07 18:01:21 -0600

    Removes 2016 announcements redirects from the front page.

--- _includes/announcement-redirect.php
+++ _includes/announcement-redirect.php
@@ -4,66 +4,6 @@
 <p>Get the latest announcements from the Heart of Texas Silhouette Association. Click on the dates for more information.
 <br>
 <br>
-<a href="announcments.php#07-27-2016" title="July 27, 2016">July 27, 2016</a>
-<br>
-VBRS Pre Registration 10/22/16 Shoot
-<br>
-<br>
-<a href="announcments.php#07-26-2016" title="July 26, 2016">July 26, 2016</a>
-<br>
-TSRA 22 BPCRA Fall Challenge
-<br>
-<br>
-<a href="announcments.php#07-25-2016" title="July 25, 2016">July 25, 2016</a>
-<br>
-2016 Texas State High Power Silhouette Championship
-<br>
-<br>
-<a href="announcments.php#07-11-2016" title="July 11, 2016">July 11, 2016</a>
-<br>
-VBRS Match Information
-<br>
-<br>
-<a href="announcments.php#07-08-2016" title="July 08, 2016">July 08, 2016</a>
-<br>
-22 BPCRA Match Information
-<br>
-<br>
-<a href="announcments.php#06-08-2016" title="June 08, 2016">June 08, 2016</a>
-<br>
-HOTSA Clays Match
-<br>
-<br>
-<a href="announcments.php#05-02-2016" title="May 02, 2016">May 02, 2016</a>
-<br>
-VBRS Information
-<br>
-<br>
-<a href="announcments.php#03-23-2016" title="March 23, 2016">March 23, 2016</a>
-<br>
-"Open Range Day"
-<br>
-<br>
-<a href="announcments.php#03-16-2016" title="March 16, 2016">March 16, 2016</a>
-<br>
-VBRS Information
-<br>
-<br>
-<a href="announcments.php#02-15-2016" title="February 15, 2016">February 15, 2016</a>
-<br>
-HOTSA Workday
-<br>
-<br>
-<a href="announcments.php#02-10-2016" title="February 10, 2016">February 10, 2016</a>
-<br>
-HOTSA Workday
-<br>
-<br>
-<a href="announcments.php#02-06-2016" title="February 06, 2016">February 06, 2016</a>
-<br>
-Annual Membership Meeting
-<br>
-<br>
 <a href="announcments.php" title="Announcements">See all <i class="fa fa-arrow-circle-o-right"></i></a></p>
 </aside><!-- #announcements-redirect -->
 </div><!-- .announcement-wrapper -->
