700ee419254a2bb6a0d3f1b3ca43cbcb397f8b7a
Kevin Sauer <kevinwsauer@users.noreply.github.com>
2017-03-15 14:17:42 -0500

    Fixes the spelling typo for announcement links

--- _includes/announcement-redirect.php
+++ _includes/announcement-redirect.php
@@ -4,46 +4,46 @@
 <p>Get the latest announcements from the Heart of Texas Silhouette Association. Click on the dates for more information.
 <br>
 <br>
-<a href="announcments.php#03-01-2017" title="March 1, 2017">March 1, 2017</a>
+<a href="announcements.php#03-01-2017" title="March 1, 2017">March 1, 2017</a>
 <br>
 Important Announcement VBRS March 25, 2017!
 <br>
 <br>
-<a href="announcments.php#02-07-2017" title="February 7, 2017">February 7, 2017</a>
+<a href="announcements.php#02-07-2017" title="February 7, 2017">February 7, 2017</a>
 <br>
 Shooting USA Episode Information
 <br>
 <br>
-<a href="announcments.php#02-03-2017" title="February 3, 2017">February 3, 2017</a>
+<a href="announcements.php#02-03-2017" title="February 3, 2017">February 3, 2017</a>
 <br>
 2017 State BPCS Match Flyer &amp; Entry Form 
 <br>
 <br>
-<a href="announcments.php#01-16-2017-0" title="January 16, 2017">January 16, 2017</a>
+<a href="announcements.php#01-16-2017-0" title="January 16, 2017">January 16, 2017</a>
 <br>
 Annual Membership Meeting - January 28th, 2017 @ 10:30 AM
 <br>
 <br>
-<a href="announcments.php#01-16-2017-1" title="January 16, 2017">January 16, 2017</a>
+<a href="announcements.php#01-16-2017-1" title="January 16, 2017">January 16, 2017</a>
 <br>
 2017 General Rules for Membership, Membership Form &amp; Release Form
 <br>
 <br>
-<a href="announcments.php#01-14-2017-0" title="January 14, 2017">January 14, 2017</a>
+<a href="announcements.php#01-14-2017-0" title="January 14, 2017">January 14, 2017</a>
 <br>
 2017 TX State BPCR Championship(s) - 22 BPCRA &amp; Big Bore - Entry Forms
 <br>
 <br>
-<a href="announcments.php#01-14-2017-1" title="January 14, 2017">January 14, 2017</a>
+<a href="announcements.php#01-14-2017-1" title="January 14, 2017">January 14, 2017</a>
 <br>
 Varmint Benchrest Silhouette (VBRS) 2017 Info &amp; Schedule
 <br>
 <br>
-<a href="announcments.php#10-23-2016" title="October 23, 2016">October 23, 2016</a>
+<a href="announcements.php#10-23-2016" title="October 23, 2016">October 23, 2016</a>
 <br>
 VBRS/Shooting USA
 <br>
 <br>
-<a href="announcments.php" title="Announcements">See all <i class="fa fa-arrow-circle-o-right"></i></a></p>
+<a href="announcements.php" title="Announcements">See all <i class="fa fa-arrow-circle-o-right"></i></a></p>
 </aside><!-- #announcements-redirect -->
 </div><!-- .announcement-wrapper -->
